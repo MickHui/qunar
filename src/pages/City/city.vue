037dc1b7d603f7773ac61e027ae87b37d1d441f2
MickMing <524238580@qq.com>
2018-11-28 14:26:02 +0800

    city-header init

--- src/pages/City/city.vue
+++ src/pages/City/city.vue
@@ -1,41 +1,25 @@
 <template>
 	<div>
-       City of Stars
+      <city-header>
+      
+      
+      </city-header>
 	</div>
 	
 </template>
 
 <script>
-	export default {
-		name:'City',
-		methods : {
-			handleBack () {
-				this.$router.push('/')
-			}
-		}
-		
-	}
+import CityHeader from './components/header'
+export default {
+	name:'City',
+	components : {
+		CityHeader
+	},
+	
+	
+}
 </script>
 
 <style lang="stylus" scoped>
-@import '~styles/varibles.styl'
-.city-header
-  display:flex
-  height:$headerHight
-  line-height:$headerHight
-  background:$bgColor
-  .header-back-button
-    line-height:$headerHight
-    padding:0 .2rem 
-    text-align:center
-    color:#fff
-    .header-back-icon
-      font-size:.36rem
-      font-weight:bolder
-  .city-header-title 
-    flex:1
-    padding-left:2.4rem
-    font-size:.36rem 
-    color:#fff
-     
+
 </style>
---
+++ src/pages/City/components/header.vue
@@ -0,0 +1,46 @@
+<template>
+	<div class="city-header">
+	    <div class="header-back-button" @click="handleBack">
+	    	<span class="iconfont header-back-icon ">&#xe625;</span>
+	    </div>
+	    <div class="city-header-title ">
+	       <span>城市选择</span>
+	    </div>
+	</div>
+	
+	
+</template>
+
+<script>
+	export default {
+		name:'CityHeader',
+		methods : {
+			handleBack () {
+				this.$router.push('/')
+			}
+	  }
+	}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+.city-header
+  display:flex
+  height:$headerHight
+  line-height:$headerHight
+  background:$bgColor
+  .header-back-button
+    line-height:$headerHight
+    padding:0 .2rem 
+    text-align:center
+    color:#fff
+    .header-back-icon
+      font-size:.36rem
+      font-weight:bolder
+  .city-header-title 
+    flex:1
+    padding-left:2.4rem
+    font-size:.36rem 
+    color:#fff
+     
+</style>
