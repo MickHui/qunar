e48326ebf5bd34421c26679dd1eb32efc305b247
MickMing <524238580@qq.com>
2018-11-29 12:32:20 +0800

    add city-search -logic

--- src/pages/City/city.vue
+++ src/pages/City/city.vue
@@ -2,8 +2,7 @@
 	<div>
       <city-header >
       </city-header>
-      <city-search>
-      </city-search>
+      <city-search :cities = 'cities'></city-search>
       <city-list 
       	:hotCities="hotCities"
       	:cities = 'cities'
--- src/pages/City/components/search.vue
+++ src/pages/City/components/search.vue
@@ -1,16 +1,77 @@
 <template>
 	<div >
 		<div class="city-search">
-		   <input class="city-search-input" type="text" placeholder="输入城市名或拼音" />
+		   <input v-model="keyword" class="city-search-input" type="text" placeholder="输入城市名或拼音" />
 		</div>
-	</div>
-	
+		<div class="search-content" v-show="keyword">
+			<ul>
+				<li class="search-list border-bottom"
+					v-for="item of list"
+					:key="item.id"
+					@click='handleClick(item.name)'
+				>
+				   {{item.name}}
+				</li>
+				<li class="search-list border-bottom"  v-if="hasNokeyword">
+					没有您要找的内容
+				</li>
+			</ul>
+		</div>
+	</div>	
 </template>
 
 <script>
-	export default {
-		name : "CitySearch"
+import Bscroll from 'better-scroll'
+export default {
+	name : "CitySearch",
+	props:{
+		cities : Object
+	},
+	data () {
+		return {
+			keyword :'',
+			list:[],
+			timer:null
+		}
+	},
+	computed:{
+		hasNokeyword () {
+			return !this.list.length	
+		}
+	},
+	watch:{
+		keyword () {
+		  if(this.timer){
+		  	clearTimeout(this.timer)
+		  }
+		  if(!this.keyword){
+		  	this.list = []
+		  }
+		  this.timer = setTimeout(()=>{
+		  	const result = []
+		  	for (let i in this.cities){
+		  		this.cities[i].forEach((value)=>{
+		  			if(value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword)>-1){
+		  				result.push(value)
+		  			}
+		  		})
+		  	}
+		  	 this.list = result
+		  },100)
+			
+		}
+	},
+	methods:{
+		handleClick (city) {
+			this.$emit("changeCity",city)
+			this.keyword = city
+			this.$router.push('/')
+		}
+	},
+	mounted () {
+		this.scroll = new Bscroll('.search-content')
 	}
+}
 </script>
 
 <style lang="stylus" scoped>
@@ -28,5 +89,19 @@
     text-align:center
     color:#666
     padding:0 .2rem
-
+.search-content
+  z-index:9
+  overflow:hidden
+  position:absolute
+  top:1.58rem
+  right:0
+  left:0
+  bottom:0
+  background:#eee
+  .search-list
+    line-height:.62rem
+    padding-left:.2rem
+    background:#fff
+    color:#666
+    
 </style>
